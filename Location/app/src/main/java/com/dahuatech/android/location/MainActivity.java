b4a5f8840ec17c3496fcbba3bcb4f03fd4d3df8c
airxiao <356321639@qq.com>
2018-01-25 23:38:52 +0800

    删除冗余代码

--- Location/app/src/main/java/com/dahuatech/android/location/MainActivity.java
+++ Location/app/src/main/java/com/dahuatech/android/location/MainActivity.java
@@ -13,11 +13,8 @@ import com.dahuatech.android.location_lbs.export.LocationProxy;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationCallBack {
 
-//    private MapView mMapview;
     private Button btn_point;
     private boolean isStartLocation = true;
-//    private AMap aMap;
-//    private CameraUpdate mCameraUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initOtherView();
-
-//        initAMap(savedInstanceState);
-
-//        initOnceGDLBS();
-
         initContinueGDLBS();
-
     }
-
-//    private void initAMap(Bundle savedInstanceState) {
-//        mMapview = (MapView) findViewById(R.id.amap);
-//        mMapview.onCreate(savedInstanceState);
-//
-//        if (aMap == null) {
-//            aMap = mMapview.getMap();
-//        }
-//    }
 
     private void initOnceGDLBS() {
         LocationProxy.get().initLocation(this, CoorType.GCJ02);
@@ -53,12 +35,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         LocationProxy.get().setLocationMode(LocMode.Device_Sensors);
         LocationProxy.get().startContinuesLoc(5 * 1000, this);
     }
-
-//    private void showMarker(LatLng latLng) {
-//        mCameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition(latLng, 18, 0, 0));
-//        aMap.moveCamera(mCameraUpdate);
-////        aMap.addMarker(new MarkerOptions().position(latLng));
-//    }
 
     private void initOtherView() {
         btn_point = (Button) findViewById(R.id.btn_point);
@@ -82,80 +58,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-//        mMapview.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        mMapview.onResume();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-//        mMapview.onSaveInstanceState(outState);
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         LocationProxy.get().stopContinuesLoc(this);
         LocationProxy.get().unInitLocation();
-//        mMapview.onDestroy();
     }
 
     @Override
     public void locationResult(LocationInfo locationInfo) {
-
+		if (locationInfo.getErrorCode() == LocationInfo.LOCATION_SUCCESS) {
+           // ...
+        }
     }
 
-//    @Override
-//    public void getLocationSuccess(AMapLocation location, GDLocationHelper.LocationQuality locationquality) {
-//        Log.d("xyl-高德", location.getAddress() + "**" + location.getLongitude() + "**" + location.getLatitude());
-//
-//        latLng = new LatLng(location.getLatitude(), location.getLongitude());
-//        latLngList.add(latLng);
-//        showMarker(latLng);
-//        aMap.addPolyline(new PolylineOptions().addAll(latLngList).width(1).color(Color.BLACK));
-//        String msg = location.getLongitude() + " ** " + location.getLatitude() + " ** " + locationquality.getGpsStatus();
-//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-//        btn_point.setText(msg);
-//    }
-
-//    @Override
-//    public void getLocationFailed(AMapLocation location, GDLocationHelper.LocationQuality locationquality) {
-//        String msg = "错误码:" + location.getErrorCode() + ",错误信息:" +
-//                location.getErrorInfo() + ",错误描述:" + location.getLocationDetail() + " ** " + locationquality.getGpsStatus();
-//        Log.d("xyl", msg);
-//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-//        btn_point.setText(msg);
-//    }
-
-//    @Override
-//    public void onNewLocation(BDLocation location) {
-//        Log.d("xyl-百度", location.getAddrStr() + "**" + location.getLongitude() + "**" + location.getLatitude()
-//         + " ** " + location.getLocType());
-//        CoordinateConverter converter = new CoordinateConverter(MainActivity.this);
-//        converter.from(CoordinateConverter.CoordType.BAIDU);
-//
-//        DPoint sourceLatlng = new DPoint(location.getLatitude(), location.getLongitude());
-//        DPoint desLatlng = null;
-//        try {
-//            converter.coord(sourceLatlng);
-//            desLatlng = converter.convert();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-//        Log.d("xyl-百度TO高德", "**" + desLatlng.getLongitude() + "**" + desLatlng.getLatitude());
-//    }
-
-//    @Override
-//    public void errorLocationMsg(String msg) {
-//        Log.d("xyl", msg);
-//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-//    }
 }
